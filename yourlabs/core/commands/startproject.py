2fbcdaf581d6961a4809b6c0371308c06b63d049
jpic <jamespic@gmail.com>
2013-09-15 05:26:53 +0200

    Removed dead code

--- yourlabs/core/commands/startproject.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.command.startproject import Command as BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, app_or_project, name, target=None, **options):
-        print 'you have', options['template']
