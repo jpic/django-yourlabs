c205cddb2c13e822cdc95e6c624cba17bde60446
jpic <jamespic@gmail.com>
2013-09-15 17:04:52 +0200

    Added a logging config for settings.vagrant

--- yourlabs/project_template/project_name/settings/vagrant.py
+++ yourlabs/project_template/project_name/settings/vagrant.py
@@ -16,3 +16,32 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': project_directory('../django.log'),
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ('logfile',),
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
