e0cd6537149d598d09b93d78a233374da7956285
jpic <jamespic@gmail.com>
2013-09-16 01:05:37 +0200

    Vagrantbootstrap rewrite: solid su calls and break up in functions.

--- yourlabs/project_template/Vagrantbootstrap.sh
+++ yourlabs/project_template/Vagrantbootstrap.sh
@@ -1,39 +1,62 @@
 #!/usr/bin/env bash
-echo export LANGUAGE=en_US.UTF-8 > /etc/bash.bashrc
-echo export LANG=en_US.UTF-8 > /etc/bash.bashrc
-echo LC_ALL=en_US.UTF-8 > /etc/bash.bashrc
-locale-gen en_US.UTF-8
-dpkg-reconfigure locales
+#echo export LANGUAGE=en_US.UTF-8 > /etc/bash.bashrc
+#echo export LANG=en_US.UTF-8 > /etc/bash.bashrc
+#echo LC_ALL=en_US.UTF-8 > /etc/bash.bashrc
+#locale-gen en_US.UTF-8
+#dpkg-reconfigure locales
 
 apt-get update
-apt-get install -y nginx uwsgi-plugin-python postgresql-9.1 python-virtualenv memcached python-imaging python-psycopg2 python-pylibmc nodejs npm git 
-npm install -g coffee-script recess
 
+touch /tmp/installscript
+chmod +x /tmp/installscript
 
-su postgres -c 'psql -f /vagrant/docs/examples/postgresql_utf8_template.sql'
-su postgres -c 'createuser -wsrd vagrant'
-su postgres -c 'createdb -E UTF8 -O vagrant django'
+function install_db {
+    sudo apt-get install postgresql-9.1
 
-mkdir -p /srv/{{ project_name }}/
-ln -sfn /vagrant /srv/{{ project_name }}/{{ project_name }}
+    cat > /tmp/installscript <<EOF
+#!/usr/bin/env bash
+psql -f /vagrant/docs/examples/postgresql_utf8_template.sql
+createuser -wsrd vagrant
+createdb -E UTF8 -O vagrant django
+EOF
+    su postgres -c /tmp/installscript
+        
+    cat > /tmp/installscript <<EOF
+export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.vagrant
+cd /srv/{{ project_name }}/{{ project_name }}
+./manage.py syncdb --noinput
+./manage.py migrate
+EOF
+    su vagrant -c /tmp/installscript
+}
 
+function install_http {
+    apt-get install -y nginx uwsgi-plugin-python nodejs npm memcached
+    npm install -g coffee-script recess
+
+    ln -sfn /vagrant/docs/examples/nginx.conf /etc/nginx/nginx.conf
+    /etc/init.d/nginx start
+    uwsgi --ini /vagrant/docs/examples/{{ project_name }}.ini
+}
+
+function install_project {
+    echo DJANGO_SETTINGS_MODULE={{ project_name }}.settings.vagrant >> /etc/bash.bashrc
+    
+    apt-get install -y git python-virtualenv python-imaging python-psycopg2 python-pylibmc
+
+    mkdir -p /srv/{{ project_name }}/ && chown vagrant /srv/{{ project_name }}
+    ln -sfn /vagrant /srv/{{ project_name }}/{{ project_name }}
+
+    cat > /tmp/installscript <<EOF
+#!/usr/bin/env bash
 virtualenv --system-site-packages /srv/{{ project_name }}/env
 source /srv/{{ project_name }}/env/bin/activate
-pip install -r /srv/{{ project_name }}/{{ project_name }}/requirements/base.txt
 pip install django
+pip install -r /srv/{{ project_name }}/{{ project_name }}/requirements/base.txt
+EOF
+    su vagrant -c /tmp/installscript
+}
 
-ln -sfn /vagrant/docs/examples/nginx.conf /etc/nginx/nginx.conf
+install_project
+install_db
 
-/etc/init.d/nginx start
-echo source /srv/{{ project_name }}/env/bin/activate >> /etc/bash.bashrc
-echo DJANGO_SETTINGS_MODULE={{ project_name }}.settings.vagrant >> /etc/bash.bashrc
-export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.vagrant
-sudo -u vagrant /srv/{{ project_name }}/env/bin/python /srv/{{ project_name }}/{{ project_name }}/manage.py syncdb --noinput
-sudo -u vagrant /srv/{{ project_name }}/env/bin/python /srv/{{ project_name }}/{{ project_name }}/manage.py migrate
-chown -R vagrant /srv/{{ project_name }}/env
-chown vagrant /srv/{{ project_name }}
-
-uwsgi --ini /vagrant/docs/examples/{{ project_name }}.ini
-
-echo Please enter password for superuser 'test'
-/srv/{{ project_name }}/{{ project_name }}/manage.py createsuperuser
