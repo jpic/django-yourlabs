d3c933d2c9b9831ebd91dc6bd71675e941a70f44
jpic <jamespic@gmail.com>
2013-11-17 17:37:41 +0100

    Removed confusing double setting

--- yourlabs/project_template/project_name/settings/prod.py
+++ yourlabs/project_template/project_name/settings/prod.py
@@ -6,10 +6,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
 FIXTURE_DIRS += (project_directory('fixtures/prod'),)
 
 CACHES = {
